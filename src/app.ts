9c0f2c0788e09a5d09b2ce1888ebd43ee4c33237
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2024-02-27 00:20:12 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
 import express from "express";
 import translateRoutes from "./Routes/translatorRoutes";
-// import itemsRouter from "./routes/itemRoutes";
 
-// Create an Express application
 const app = express();
 
 app.use(express.json());
-
-
-// Home route
 app.get("/", (req, res) => {
     res.send("Welcome! to translate....");
 });
@@ -17,13 +12,9 @@ app.use("/translate", translateRoutes);
 
 // Define the port to listen on, defaulting to 50000 if not specified in the environment
 const PORT = 5000;
-
-
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 });
-
-
 
 // Handle uncaught exceptions and unhandled rejections
 process.on("uncaughtException", (err) => {
